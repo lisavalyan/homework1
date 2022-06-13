e0c5b2e58b5356968bc5f682177970f148c35bc3
elizaveta <lizanaj3@gmail.com>
2022-06-13 16:04:22 +0700

    Выполнение дз

--- Sem1/Program.cs
+++ Sem1/Program.cs
@@ -1,2 +1,98 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+int a , b, max, min;
+
+Console.WriteLine("Введите первое число");
+a=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+b=Convert.ToInt32(Console.ReadLine());
+{
+if(a > b)
+{
+max=a;
+min=b;
+}
+else
+{
+max=b;
+min=a;
+}
+}
+Console.WriteLine("Максимальное число " + max + " и минимальное число " + min);
+*/
+
+
+
+
+/*
+int a , b, c, max;
+
+Console.WriteLine("Введите первое число");
+a=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+b=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+c=Convert.ToInt32(Console.ReadLine());
+
+max=a;
+{
+if(b>max)
+{
+max=b;
+}
+if(c>max)
+{
+max=c;
+}
+}
+Console.WriteLine("Максимальное число " + max );
+*/
+
+
+
+
+
+/*
+int a, num;
+
+Console.WriteLine("Введите число");
+a=Convert.ToInt32(Console.ReadLine());
+
+num=a%2;
+
+{
+    if(num<1)
+    {
+        Console.WriteLine("Число является четным");
+    }
+    else
+    {
+         Console.WriteLine("Число не является четным");
+    } 
+}
+*/
+
+
+
+
+/*
+int a;
+
+Console.WriteLine("Введите число");
+a=Convert.ToInt32(Console.ReadLine());
+
+int num=2;
+
+while(num<=a)
+{
+    int result=num%2;
+
+    if(result<1)
+    {
+        Console.Write( num + " ");
+    }
+    num++;
+}
+*/
